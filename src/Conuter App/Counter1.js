ff56d4d9833ed62bc40a4057aef49bbe231a791c
Muhammad Awais <awaisiqbal78692110@gmail.com>
2022-03-08 15:42:56 +0500

    counter app

--- src/Conuter App/Counter1.js
+++ src/Conuter App/Counter1.js
@@ -1,17 +1,33 @@
+import React, { useState } from "react";
 function Counter1() {
   let counter = 0;
+  const [counter2, setCounter] = useState(0);
+  const [name, setName] = useState("Awais");
+  const [age, setAge] = useState(18);
+  const [price, setPrice] = useState(34.45);
   const incrementHandler = () => {
     counter++;
-    alert("Increment" + counter);
+    // alert("Increment" + counter);
+    setCounter(counter2 + 1);
   };
   const decrementHandler = () => {
     counter--;
-    alert("Decrement" + counter);
+    // alert("Decrement" + counter);
+    setCounter(counter2 - 1);
   };
+  console.log("Counter1", counter);
+  console.log("Counter2>>", counter2);
+
   return (
     <div style={{ padding: 50 }}>
+      Using Simple Variable:
       <button onClick={decrementHandler}>-</button>
       <span style={{ padding: 20 }}>Counter is : {counter}</span>
+      <button onClick={incrementHandler}>+</button>
+      <hr />
+      Using React State:
+      <button onClick={decrementHandler}>-</button>
+      <span style={{ padding: 20 }}>Counter is : {counter2}</span>
       <button onClick={incrementHandler}>+</button>
     </div>
   );
